baaf66c4e11115d9dda5a18fbafa28ff2836dde5
Dave Sescleifer <dave@sescleifer.com>
2012-11-04 15:37:19 -0600

    Pinch-to-zoom gesture added

--- board.lua
+++ board.lua
@@ -1,21 +1,72 @@
+system.activate("multitouch")
+
 local storyboard = require( "storyboard" )
 local scene = storyboard.newScene()
-local board, boardStartX, boardStartY = nil
+local board = nil
 
-local onTouch = function( event )
-  if event.phase == "began" then
-    boardStartX, boardStartY = board.x, board.y
+function definitions()
+  -- Listener Function for Touch Events on the Board
+  function board:touch( event )
+    if not self.touches then
+      self.touches = {}
+    end
+    
+    if event.phase == "cancelled" or event.phase == "ended" then
+      self.lastDistance = nil
+      if self.touches[1].id == event.id then
+        self.touches[1] = self.touches[2]
+        self.touches[2] = nil
+      else
+        self.touches[2] = nil
+      end
+    end
+
+    if event.phase == "began" then
+      if #self.touches == 0 then
+        self.touches[1] = event
+        self.startX, self.startY = board.x, board.y
+      elseif #self.touches == 2 then
+        self.touches[1] = event
+      else
+        self.touches[2] = event
+      end
+    end
+
+    if event.phase == "moved" then
+      if self.touches[1].id == event.id then
+        self.touches[1] = event
+      else
+        self.touches[2] = event
+      end
+
+      if #self.touches == 1 then
+        -- Dragging
+        board.x = self.startX + event.x - event.xStart
+        board.y = self.startY + event.y - event.yStart
+      else
+        -- Pinch to Zoom Resizing
+        x1, x2, y1, y2 = self.touches[1].x, self.touches[2].x, self.touches[1].y, self.touches[2].y
+        distance = math.sqrt( math.pow( x1 - x2, 2 ) + math.pow( y1 - y2, 2 ) )
+
+        if self.lastDistance then
+          dd = distance - self.lastDistance
+          
+          self.xScale = self.xScale + dd / 300
+          self.yScale = self.yScale + dd / 300
+          print(self.xScale)
+        end
+
+        self.lastDistance = distance
+      end
+    end
   end
-
-  board.x = boardStartX + event.x - event.xStart
-  board.y = boardStartY + event.y - event.yStart
 end
 
 function scene:createScene( event )
 	local group = self.view
 
   -- Create a solid white background
-  display.newRect( 0, 0, display.contentWidth, display.contentHeight )
+  background = display.newRect( 0, 0, display.contentWidth, display.contentHeight )
 
   board = display.newImage( "board.png" )
   board.width = 300
@@ -23,7 +74,12 @@ function scene:createScene( event )
   -- Center the board
   board.x = display.contentWidth / 2
   board.y = display.contentHeight / 2
-  Runtime:addEventListener( "touch", onTouch )
+  group:addEventListener( "touch", board )
+
+  definitions()
+
+  group:insert( background )
+  group:insert( board )
 end
 
 function scene:enterScene( event )
